148303fadf77125bc550667511947147953c0865
Adam Cohen-Rose <adam.cohen-rose@tesco.com>
2018-03-15 15:03:10 +0000

    Tested working on Linux
    * using `yes` instead of `echo y` to accept licenses

--- lib/fastlane/plugin/android_sdk_update/actions/android_sdk_update_action.rb
+++ lib/fastlane/plugin/android_sdk_update/actions/android_sdk_update_action.rb
@@ -12,10 +12,17 @@ module Fastlane
           Actions::BrewAction.run(command: "cask ls --versions android-sdk || brew cask install android-sdk")
           sdk_path = File.realpath("../..", FastlaneCore::CommandExecutor.which("android"))
         elsif FastlaneCore::Helper.linux?
-          sdk_path = params[:linux_sdk_install_dir]
-          if !File.exist?("#{sdk_path}/tools/android")
-            Actions.sh("wget #{params[:linux_sdk_download_url]}")
-            Actions.sh("unzip sdk-tools-linux-3859397.zip -d $HOME/android-sdk && rm sdk-tools-linux-3859397.zip")
+          sdk_path = File.expand_path(params[:linux_sdk_install_dir])
+          if File.exist?("#{sdk_path}/tools/android")
+            UI.message("Using existing android-sdk at #{sdk_path}")
+          else
+            UI.message("Downloading android-sdk to #{sdk_path}")
+            FastlaneCore::CommandExecutor.execute(
+              command: "/usr/bin/wget -O /tmp/android-sdk-tools.zip #{params[:linux_sdk_download_url]}",
+              print_all: true, print_command: true)
+            FastlaneCore::CommandExecutor.execute(
+              command: "/usr/bin/unzip /tmp/android-sdk-tools.zip -d #{sdk_path}",
+              print_all: true, print_command: true)
           end
         else
           UI.user_error! 'Your OS is currently not supported.'
@@ -42,23 +49,26 @@ module Fastlane
         unless File.exist?(sdk_manager)
           # In case an old SDK is installed without the sdkmanager
           UI.important("Installed Android-SDK tools are outdated.")
-          Actions.sh "echo y | android update sdk --no-ui --all --filter tools"
+          Actions.sh "yes | android update sdk --no-ui --all --filter tools"
         end
 
         # Accept licenses for all available packages
-        FastlaneCore::CommandExecutor.execute(command: "echo y | #{sdk_manager} --licenses",
+        UI.important("Accepting licenses on your behalf!")
+        FastlaneCore::CommandExecutor.execute(command: "yes | #{sdk_manager} --licenses",
                                               print_all: true,
                                               print_command: false)
 
         # Install packages
+        UI.message("Installing packages...")
         packages.each { |package| UI.message("• #{package}") }
-        FastlaneCore::CommandExecutor.execute(command: "echo y | #{sdk_manager} '#{packages.join("' '")}'",
+        FastlaneCore::CommandExecutor.execute(command: "yes | #{sdk_manager} '#{packages.join("' '")}'",
                                               print_all: true,
                                               print_command: false)
 
         if params[:update_installed_packages]
+          UI.message("Updating all installed packages")
           # Ensure all installed packages are updated
-          FastlaneCore::CommandExecutor.execute(command: "echo y | #{sdk_manager} --update",
+          FastlaneCore::CommandExecutor.execute(command: "yes | #{sdk_manager} --update",
                                                 print_all: true,
                                                 print_command: false)
         end
@@ -104,7 +114,7 @@ module Fastlane
                                        env_name: "FL_ANDROID_LINUX_SDK_INSTALL_DIR",
                                        description: "Install directory for Android SDK on Linux",
                                        optional: true,
-                                       default_value: "$HOME/android-sdk"),
+                                       default_value: "~/android-sdk"),
           FastlaneCore::ConfigItem.new(key: :linux_sdk_download_url,
                                        env_name: "FL_ANDROID_LINUX_SDK_DOWNLOAD_URL",
                                        description: "Download URL for Android SDK on Linux",
